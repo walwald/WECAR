a0b2e7e34e6b2734fe9749d656eefaa3d59b111e
dahee jang <wol04255@gmail.com>
2023-05-31 15:20:12 +0900

    wip

--- src/auth/security/auth.guard.ts
+++ src/auth/security/auth.guard.ts
@@ -1,12 +1,28 @@
-import { ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { AuthGuard as NestAuthGuard } from '@nestjs/passport';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthGuard extends NestAuthGuard('jwt') {
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
+  canActivate(context: ExecutionContext) {
     return super.canActivate(context);
+  }
+
+  // canActivate(
+  //   context: ExecutionContext,
+  // ): boolean | Promise<boolean> | Observable<boolean> {
+  //   return super.canActivate(context);
+  // }
+  handleRequest(err: any, user: any) {
+    // 에러가 발생하거나 토큰이 만료된 경우 여기에서 처리합니다.
+    console.log('여기는 authguard: ', err);
+    if (err || !user) {
+      throw err || new UnauthorizedException('Token has expired');
+    }
+    return user;
   }
 }
